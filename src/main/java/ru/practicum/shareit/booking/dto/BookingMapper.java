53c824957047f87183f675374d7820531c4262d1
DembitskaiaAA <tsutskova.an1996@yandex.ru>
2023-06-16 00:16:39 +0300

    refactor: добавить аннотацию для ограничения создания экземпляров класса

--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -1,9 +1,12 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.ItemBookerDto;
 import ru.practicum.shareit.user.dto.UserBookerDto;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BookingMapper {
     public static Booking toBooking(BookingInputDto bookingInputDto) {
         return new Booking(
