e149b68ba5847bd99b417f832b3bd509e70e419e
DembitskaiaAA <tsutskova.an1996@yandex.ru>
2023-06-21 15:55:31 +0300

    refactor: изменить маппер

--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -1,37 +1,36 @@
 package ru.practicum.shareit.booking.dto;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.ItemBookerDto;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.dto.UserBookerDto;
+import ru.practicum.shareit.user.model.User;
 
-public class BookingMapper {
-    public static Booking toBooking(BookingInputDto bookingInputDto) {
-        return new Booking(
-                bookingInputDto.getStart(),
-                bookingInputDto.getEnd()
-        );
+@Component
+@Mapper(componentModel = "spring")
+public abstract class BookingMapper {
+
+    public abstract Booking transformBookingInputDtoToBooking(BookingInputDto bookingInputDto);
+
+    @Mapping(target = "booker", expression = "java(mapToBooker(booking.getBooker()))")
+    @Mapping(target = "item", expression = "java(mapToItem(booking.getItem()))")
+    public abstract BookingOutputDto transformBookingToBookingOutputDto(Booking booking);
+
+    UserBookerDto mapToBooker(User booker) {
+        return new UserBookerDto(booker.getId());
     }
 
-    public static BookingOutputDto toBookingDto(Booking booking) {
-        return new BookingOutputDto(
-                booking.getId(),
-                booking.getStart(),
-                booking.getEnd(),
-                booking.getStatus(),
-                new UserBookerDto(booking.getBooker().getId()),
-                new ItemBookerDto(booking.getItem().getId(),
-                        booking.getItem().getName())
-        );
+    ItemBookerDto mapToItem(Item item) {
+        return new ItemBookerDto(item.getId(), item.getName());
     }
 
-    public static BookingItemDto toBookingItemDto(Booking booking) {
-        if (booking == null) {
-            return null;
-        } else {
-            return new BookingItemDto(
-                    booking.getId(),
-                    booking.getBooker().getId()
-            );
-        }
+    @Mapping(target = "bookerId", expression = "java(mapToBookerId(booking.getBooker()))")
+    public abstract BookingItemDto transformBookingToBookingItemDto(Booking booking);
+
+    Long mapToBookerId(User booker) {
+        return booker.getId();
     }
 }
