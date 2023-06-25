b59d532d760827db5404dcb866da408740e3e67d
DembitskaiaAA <tsutskova.an1996@yandex.ru>
2023-06-26 00:53:00 +0300

    refactor: использовать entityManager вместо repository

--- src/test/java/ru/practicum/shareit/request/repository/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/repository/ItemRequestRepositoryTest.java
@@ -3,13 +3,12 @@ package ru.practicum.shareit.request.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
-import ru.practicum.shareit.user.repository.UserRepository;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,23 +17,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @DataJpaTest
 @ActiveProfiles("test")
 class ItemRequestRepositoryTest {
-
+    @Autowired
+    private TestEntityManager entityManager;
     @Autowired
     private ItemRequestRepository itemRequestRepository;
-    @Autowired
-    private UserRepository userRepository;
 
     @Test
     void test_findAllByRequestorIdOrderByCreatedDesc() {
-        User requester = new User(1L, "John Doe", "john@email.com");
-        userRepository.save(requester);
+        User requester = new User(null, "John Doe", "john@email.com");
+        entityManager.persist(requester);
         LocalDateTime now = LocalDateTime.now();
 
-        ItemRequest request1 = new ItemRequest(1L, "Request 1", requester, now.minusDays(2));
-        ItemRequest request2 = new ItemRequest(2L, "Request 2", requester, now.minusDays(1));
-        ItemRequest request3 = new ItemRequest(3L, "Request 3", requester, now);
-
-        itemRequestRepository.saveAll(Arrays.asList(request1, request2, request3));
+        ItemRequest request1 = new ItemRequest(null, "Request 1", requester, now.minusDays(2));
+        ItemRequest request2 = new ItemRequest(null, "Request 2", requester, now.minusDays(1));
+        ItemRequest request3 = new ItemRequest(null, "Request 3", requester, now);
+        entityManager.merge(request1);
+        entityManager.merge(request2);
+        entityManager.merge(request3);
+        entityManager.flush();
 
         Long requestorId = 1L;
         List<ItemRequest> requests = itemRequestRepository.findAllByRequestorIdOrderByCreatedDesc(requestorId);
